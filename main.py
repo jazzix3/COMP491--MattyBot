f115d5fe26f52e6204892e400a3c66c4f027459c
Jasmine Maquindang <jazzix3@gmail.com>
2023-02-13 16:11:57 -0800

    implemented deleteevent, embeds for response msgs

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ dotenv.load_dotenv()
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
 
-
-
 class Client(discord.Client):
   def __init__(self) -> None:
       intents = discord.Intents.default()
@@ -60,8 +58,14 @@ class AddFaqModal(discord.ui.Modal, title='Add to FAQ'):
       cursor.execute(sql, val)
       db.commit()
       db.close()
-      await interaction.response.send_message(f'Success! A new FAQ has been added!\n\nQuestion: **{self.question}**\nAnswer: **{self.answer}**', ephemeral=True)
+      
+      embed = discord.Embed(title="Success! A new FAQ has been added.", description="", color = discord.Color.green())
+      embed.add_field(name="Question", value=f'{self.question}', inline=False)
+      embed.add_field(name="Answer", value=f'{self.answer}', inline=False)
 
+      await interaction.response.send_message(embed=embed, ephemeral=True)
+
+      
     async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
       await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
 
@@ -98,9 +102,12 @@ class AddEventModal(discord.ui.Modal, title='Add an Event'):
 client = Client()
 
 
+### FAQ COMMANDS ###
+
 @client.tree.command(name="addfaq", description="Add a question & answer to FAQ")
 async def addfaq(interaction: discord.Interaction):
     await interaction.response.send_modal(AddFaqModal())
+
 
 @client.tree.command(name="listfaq", description="View a list of all FAQ")
 async def listfaq(interaction:discord.Interaction):
@@ -120,35 +127,6 @@ async def listfaq(interaction:discord.Interaction):
   await interaction.response.send_message(embed=embed, ephemeral=True)
   db.commit()
   db.close()
-
-
-@client.tree.command(name="addevent", description="Add an event")
-async def addevent(interaction: discord.Interaction):
-    await interaction.response.send_modal(AddEventModal())
-
-
-@client.tree.command(name="listevents", description="View a list of all Events")
-async def listevents(interaction:discord.Interaction):
-  db = sqlite3.connect('db.sqlite')
-  cursor = db.cursor()
-  cursor.execute('''
-    SELECT name, date, time FROM events_db
-      ''')
-  embed = discord.Embed(title="List of all events", description="For more information about an event, -- **directions to do something** --", color = discord.Color.blue())
-  rows = cursor.fetchall()
-  count = 1
-  for row in rows:
-    name = row[0]
-    date = row[1]
-    time = row[2]
-    embed.add_field(name=f'{count}- {name}', value=f'{date} at {time}', inline= False)
-    count += 1
-  
-  await interaction.response.send_message(embed=embed, ephemeral=True)
-  db.commit()
-  db.close()
-
-
 
 
 @client.tree.command(name="listanswers", description="View a list of all answers from FAQ")
@@ -178,19 +156,9 @@ async def deleteallfaq(interaction: discord.Interaction):
   cursor.execute('''
     DELETE FROM faq_db
       ''')
-  await interaction.response.send_message("Done! All FAQ have been cleared from the database")
-  db.commit()
-  db.close()
 
-
-@client.tree.command(name="deleteallevents", description="Delete all events from the database")
-async def deleteallevents(interaction: discord.Interaction):
-  db = sqlite3.connect('db.sqlite')
-  cursor = db.cursor()
-  cursor.execute('''
-    DELETE FROM events_db
-      ''')  
-  await interaction.response.send_message("Done! All events have been cleared from the database")
+  embed = discord.Embed(title="Success! All FAQ have been cleared from the database", description="", color = discord.Color.green())
+  await interaction.response.send_message(embed=embed, ephemeral=True)
   db.commit()
   db.close()
 
@@ -202,14 +170,14 @@ async def deletefaq(interaction: discord.Interaction):
   cursor.execute('''
     SELECT questions FROM faq_db
       ''')
-  embed = discord.Embed(title="Delete a FAQ", description="Enter the number of the FAQ to delete", color = discord.Color.blue())
+  embed = discord.Embed(title="Delete a FAQ", description="Enter the number of the FAQ to delete", color = discord.Color.orange())
   rows = cursor.fetchall()
   count = 1
   for row in rows:
     question = row[0]
     embed.add_field(name=f'{count}- {question}', value='\n', inline= False)
     count += 1
-  await interaction.response.send_message(embed=embed)
+  await interaction.response.send_message(embed=embed, ephemeral=True)
 
   def check(m):
       return m.author == interaction.user and m.channel == interaction.channel
@@ -219,22 +187,86 @@ async def deletefaq(interaction: discord.Interaction):
   selected_row = rows[selected_index]
   cursor.execute('''DELETE FROM faq_db WHERE questions=?''', (selected_row[0],))
   
-  await interaction.channel.send(f"Done! You deleted FAQ #{int(selection.content)}.")
+  embed2 = discord.Embed(title=f"Success! FAQ #{int(selection.content)}- {question} has been deleted.", description="", color = discord.Color.green())
+  await interaction.followup.send(embed=embed2, ephemeral=True)
+  db.commit()
+  db.close() 
+
+
+
+
+### EVENT COMMANDS ###
+
+@client.tree.command(name="addevent", description="Add an event")
+async def addevent(interaction: discord.Interaction):
+    await interaction.response.send_modal(AddEventModal())
+
+
+@client.tree.command(name="listevents", description="View a list of all Events")
+async def listevents(interaction:discord.Interaction):
+  db = sqlite3.connect('db.sqlite')
+  cursor = db.cursor()
+  cursor.execute('''
+    SELECT name, date, time FROM events_db
+      ''')
+  embed = discord.Embed(title="List of all events", description="For more information about an event, -- **directions to do something** --", color = discord.Color.blue())
+  rows = cursor.fetchall()
+  count = 1
+  for row in rows:
+    name = row[0]
+    date = row[1]
+    time = row[2]
+    embed.add_field(name=f'{count}- {name}', value=f'{date} at {time}', inline= False)
+    count += 1
+  
+  await interaction.response.send_message(embed=embed, ephemeral=True)
   db.commit()
   db.close()
+
+
+@client.tree.command(name="deleteallevents", description="Delete all events from the database")
+async def deleteallevents(interaction: discord.Interaction):
+  db = sqlite3.connect('db.sqlite')
+  cursor = db.cursor()
+  cursor.execute('''
+    DELETE FROM events_db
+      ''')
+
+  embed = discord.Embed(title="Success! All events have been cleared from the database", description="", color = discord.Color.green())
+  await interaction.response.send_message(embed=embed, ephemeral=True)
+  db.commit()
+  db.close()
+
+
+@client.tree.command(name="deleteevent", description = "Delete an event from the database")
+async def deleteevent(interaction: discord.Interaction):
+  db = sqlite3.connect('db.sqlite')
+  cursor = db.cursor()
+  cursor.execute('''
+    SELECT name FROM events_db
+      ''')
+  embed = discord.Embed(title="Delete an event", description="Enter the number of the event to delete", color = discord.Color.orange())
+  rows = cursor.fetchall()
+  count = 1
+  for row in rows:
+    name = row[0]
+    embed.add_field(name=f'{count}- {name}', value=f'\n', inline= False)
+    count += 1
   
+  await interaction.response.send_message(embed=embed, ephemeral=True)
 
+  def check(m):
+    return m.author == interaction.user and m.channel == interaction.channel
 
+  selection = await client.wait_for('message', check=check)
+  selected_index = int(selection.content) - 1
+  selected_row = rows[selected_index]
+  cursor.execute('''DELETE FROM events_db WHERE name=?''', (selected_row[0],))
 
-  
-
-
-
-
-
-    
-
-
+  embed2 = discord.Embed(title=f"Success! Event #{int(selection.content)}- {name} has been deleted.", description="", color = discord.Color.green())
+  await interaction.followup.send(embed=embed2, ephemeral=True)
+  db.commit()
+  db.close() 
 
 
 client.run(TOKEN)
