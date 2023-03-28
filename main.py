34c5b542ee9f104b98d706d5ba7ceeb572c94b77
Jasmine Maquindang <jazzix3@gmail.com>
2023-03-28 16:09:57 -0700

    create event example

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import os
+import os, os.path
 import discord
 import dotenv
 import time
 from discord.ext import commands
 from matty_db import Database
 from colorama import Back, Fore, Style
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+#from googleapiclient.errors import HttpError
+
+
+
 
 
 
@@ -22,13 +30,45 @@ class Client(commands.Bot):
             await self.load_extension(cog)
 
     async def on_ready(self):
-        prfx = (Style.BRIGHT + Back.BLACK + Fore.GREEN + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + Back.RESET + Fore.WHITE)
-        print(prfx + " Logged in as " + Fore.YELLOW + self.user.name)
-        print(prfx + " Bot ID " + Fore.YELLOW + str(self.user.id))
+        prfx = (Fore.WHITE + Style.DIM + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + Back.RESET )
+        print(prfx + Style.NORMAL +" Logged in as "  + Fore.YELLOW + self.user.name)
+        print(prfx + Style.NORMAL + " Bot ID "  + Fore.YELLOW + str(self.user.id))
         synced = await self.tree.sync()
-        print(prfx + " Number of cogs loaded: " + Fore.YELLOW + str(len(self.coglist)) )
-        print(prfx + " Number of slash commands synced: " + Fore.YELLOW + str(len(synced)))
-        
+        print(prfx + Style.NORMAL + " Number of cogs loaded: "  + Fore.YELLOW + str(len(self.coglist)))
+        #print(prfx + " Number of slash commands synced: " + Fore.YELLOW + str(len(synced)))
+        runCalendar()
+        print(prfx + Style.NORMAL + " Logged into Google Calendar id: "  + Fore.YELLOW + "***(will add id later)***")
+            
+
+
+
+
+
+def runCalendar():
+    creds = None
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+    if os.path.exists('cal_token.json'):
+        creds = Credentials.from_authorized_user_file('cal_token.json', SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'cal_credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('cal_token.json', 'w') as token:
+            token.write(creds.to_json())
+
+
+ 
+
+    
+
+   
+
 
 
 dotenv.load_dotenv()
